cb12f7af9364fd7107f107c88fab419512cb0dbf
vixhnuchandran <vixhnuchandran@gmail.com>
2024-01-10 19:54:05 +0530

    feat: video selection functionality in js/main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const videoDropDown = document.getElementById("videoDropdown");
+  const videoPlayer = document.getElementById("videoPlayer");
+
+  videoDropDown.addEventListener("change", () => {
+    const selectedVideo = videoDropDown.value;
+    videoPlayer.src = `./` + selectedVideo;
+  });
+});
