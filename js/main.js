0a45607ab437b30b7518ae9b5163d627bfc84cc7
vixhnuchandran <vixhnuchandran@gmail.com>
2024-01-23 20:47:07 +0530

    re arrange files, and inlcuded style and script inside indexhtl

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const videoDropDown = document.getElementById("videoDropdown")
-  const videoPlayer = document.getElementById("videoPlayer")
-
-  videoDropDown.addEventListener("change", () => {
-    const selectedVideo = videoDropDown.value
-    videoPlayer.src = `./` + selectedVideo
-  })
-})
